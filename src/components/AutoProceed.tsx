948433229fe8c791e3bc282cdd3ce72fa9cdedbd
IndexOfNull <21084775+IndexOfNull@users.noreply.github.com>
2021-12-11 19:24:43 -0600

    Fix AutoProceed effect
    This makes AutoProceed set its index (current element) back to zero, which enables other AutoProceed elements to be stacked with nothing in between.

--- src/components/AutoProceed.tsx
+++ src/components/AutoProceed.tsx
@@ -1,44 +1,64 @@
-import React, { ReactNode, useEffect, useState } from "react"
+import React, { ReactNode, useEffect, useState } from 'react';
 
 type AutoProceedProps = {
-    duration?: number
-    children?: ReactNode
-    onFinish?: Function
-}
+  duration?: number;
+  children?: ReactNode;
+  onFinish?: Function;
+  onChange?: Function;
+};
 
+export const AutoProceed = ({
+  duration = 1000,
+  children,
+  onFinish = () => {},
+  onChange = () => {},
+}: AutoProceedProps) => {
+  const childrenArray = React.Children.toArray(children);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-export const AutoProceed = ({duration = 1000, children, onFinish = () => {}}: AutoProceedProps) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (currentIndex < childrenArray.length - 1) {
+        setCurrentIndex(currentIndex + 1);
+        onChange();
+      } else {
+        onFinish();
+        onChange();
+        setCurrentIndex(0); //Super hacky but this allows us to stack AutoProceed components together
+      }
+    }, duration);
 
-    const childrenArray = React.Children.toArray(children).map((child) => {
-        if (child != ",") {
-            return child
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [currentIndex]);
+
+  /* useEffect(() => {
+    console.log('Effecting');
+    if (childrenArray.length == 0) {
+      setTimeout(onFinish, duration);
+    } else {
+      let counter = 0;
+      const interval = setInterval(() => {
+        setCurrentIndex(counter);
+        counter++;
+        if (counter == childrenArray.length) {
+          clearInterval(interval);
+          onFinish();
         }
-    });
-    const [currentIndex, setCurrentIndex] = useState(0);
+      }, duration);
 
-    useEffect(() => {
-        let counter = 1;
-        const interval = setInterval(() => {
-            if (counter == childrenArray.length) {
-                clearInterval(interval);
-                onFinish()
-            } else {
-                setCurrentIndex(counter);
-                counter++;
-            }
-        }, duration)
+      return () => clearInterval(interval);
+    }
+  }, []); */
 
-        return () => clearInterval(interval);
-    }, [])
-
-    return (
-        <div>
-            {childrenArray.map((child, index) => {
-                if (currentIndex == index) {
-                    return (child)
-                }
-            })}
-        </div>
-    )
-
-}
+  return (
+    <div>
+      {childrenArray.map((child, index) => {
+        if (currentIndex == index) {
+          return child;
+        }
+      })}
+    </div>
+  );
+};
